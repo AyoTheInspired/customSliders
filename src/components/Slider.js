e6c8faa58008b7a44edc0347a2024b474d262b39
Ayo <ayoinspired@gmail.com>
2022-11-04 15:36:39 +0100

    App is complete

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,15 +1,52 @@
-import {FlatList, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Animated, FlatList, StyleSheet, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
 import Slides from '../data';
 import SlideItem from './SlideItem';
+import Pagination from './Pagination';
 
 const Slider = () => {
+  const [index, setIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const handleScroll = event => {
+    Animated.event(
+      [
+        {
+          nativeEvent: {
+            contentOffset: {
+              x: scrollX,
+            },
+          },
+        },
+      ],
+      {
+        useNativeDriver: false,
+      },
+    )(event);
+  };
+
+  const handleOnViewableItemsChanged = useRef(({viewableItems}) => {
+    setIndex(viewableItems[0].index);
+  }).current;
+
+  const viewabilityConfig = useRef({
+    itemVisiblePercentThreshold: 50,
+  }).current;
+
   return (
     <View>
       <FlatList
+        horizontal
+        pagingEnabled
+        snapToAlignment="center"
+        showsHorizontalScrollIndicator={false}
         data={Slides}
         renderItem={({item}) => <SlideItem item={item} />}
+        onScroll={handleScroll}
+        onViewableItemsChanged={handleOnViewableItemsChanged}
+        viewabilityConfig={viewabilityConfig}
       />
+      <Pagination data={Slides} scrollX={scrollX} index={index} />
     </View>
   );
 };
